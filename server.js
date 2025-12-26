18f5c69b3c064a8736f176c87c6ff07b9a8a4112
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-26 15:28:07 +0200

    Integrate MongoDB connection with mongoose
    Added MongoDB connection using mongoose.

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 app.use(express.json());
 
+// 🔗 الاتصال بقاعدة البيانات
+mongoose
+  .connect(process.env.MONGO_URI || "mongodb://localhost:27017/pos", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Route تجريبي
 app.get("/", (req, res) => {
   res.send("POS Backend is running");
 });
